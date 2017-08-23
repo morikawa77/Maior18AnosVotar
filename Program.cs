44c0bc918ab56b35c78ee48ab6577e29082a59f8
Reginaldo Morikawa <reginaldo@wolfandpartners.com.br>
2017-08-23 06:55:45 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Maior18AnosVotar
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			int idade;
+            string nome;
+
+            Console.Write("Digite seu nome: ");
+            nome = Console.ReadLine();
+			Console.Write("Digite sua idade: ");
+			idade = Convert.ToInt32(Console.ReadLine());
+			if (idade < 18)
+			{
+                Console.Write("{0}, você é menor de 18 anos.", nome);
+            } else {
+                Console.Write("{0}, você é maior de 18 anos, já pode dirigir e é obrigado a votar.", nome);
+            }
+			Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+If Maior de 18 anos pode votar C# Curso Programação de Dispositivos Móveis SENAC
